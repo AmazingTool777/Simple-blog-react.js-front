2269a78388b355feeaa2b31230646977321c8505
Tolotra Rabefaly <tolotor@yahoo.fr>
2022-05-14 22:08:32 +0300

    Posts data custom hook integration

--- src/pages/PostsPage/PostsPage.jsx
+++ src/pages/PostsPage/PostsPage.jsx
@@ -3,7 +3,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // Components
 import PostsParams from "./PostsParams";
 
+// Custom hooks
+import usePosts from "./usePosts";
+
 const PostsPage = () => {
+	const { order, handleOrderChange, handleSearchChange, isLoading } = usePosts();
+
 	return (
 		<section id="posts-page">
 			<header className="mb-5">
@@ -13,7 +18,12 @@ const PostsPage = () => {
 				</h1>
 				<hr />
 			</header>
-			<PostsParams order="desc" onOrderChange={() => {}} onSearchSubmit={() => {}} />
+			<PostsParams
+				isDisabled={isLoading}
+				order={order}
+				onOrderChange={handleOrderChange}
+				onSearchSubmit={handleSearchChange}
+			/>
 		</section>
 	);
 };
