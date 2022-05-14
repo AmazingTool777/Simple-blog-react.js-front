88da0f3b58ce04a7c2517473fe4d2e5b3ecc1441
Tolotra Rabefaly <tolotor@yahoo.fr>
2022-05-14 21:23:40 +0300

    Application navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { BrowserRouter as Router } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 // Components
 import IconsImports from "./components/IconsImports";
+import AppNavbar from "./components/AppNavbar";
 
 function App() {
 	return (
 		<IconsImports>
-			<div className="App"></div>
+			<div className="App">
+				<Router>
+					<AppNavbar />
+				</Router>
+			</div>
 		</IconsImports>
 	);
 }
---
+++ src/components/AppNavbar/AppNavbar.jsx
@@ -0,0 +1,43 @@
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Nav from "react-bootstrap/Nav";
+import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const AppNavbar = () => {
+	return (
+		<Navbar bg="primary" variant="dark" expand="md" sticky="top" className="shadow-sm">
+			<div className="container">
+				<Navbar.Brand as={NavLink} to="/">
+					📰 Simple-blog
+				</Navbar.Brand>
+				<Navbar.Collapse>
+					<Nav className="ms-auto">
+						<Nav.Link as={NavLink} to="/posts">
+							Posts
+						</Nav.Link>
+						<Nav.Link as={NavLink} to="/add-post">
+							Add a post
+						</Nav.Link>
+						<Nav.Link as={NavLink} to="/users">
+							Users
+						</Nav.Link>
+						<NavDropdown title="Authentication">
+							<NavDropdown.Item as={NavLink} to="/signup">
+								<FontAwesomeIcon icon="user-plus" className="me-2" />
+								Sign up
+							</NavDropdown.Item>
+							<NavDropdown.Divider />
+							<NavDropdown.Item as={NavLink} to="/signup">
+								<FontAwesomeIcon icon="lock" className="me-2" />
+								Log in
+							</NavDropdown.Item>
+						</NavDropdown>
+					</Nav>
+				</Navbar.Collapse>
+			</div>
+		</Navbar>
+	);
+};
+
+export default AppNavbar;
