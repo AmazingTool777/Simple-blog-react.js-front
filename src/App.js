754196ff64342406f26902fb77464475a356c426
Tolotra Rabefaly <tolotor@yahoo.fr>
2022-05-14 21:31:02 +0300

    Posts page and route initial setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 // Components
 import IconsImports from "./components/IconsImports";
 import AppNavbar from "./components/AppNavbar";
+import PostsPage from "./pages/PostsPage";
 
 function App() {
 	return (
@@ -11,6 +12,12 @@ function App() {
 			<div className="App">
 				<Router>
 					<AppNavbar />
+					<div className="container pt-4 pb-5">
+						<Routes>
+							<Route path="/" element={<Navigate to="/posts" replace />} />
+							<Route path="/posts" element={<PostsPage />} />
+						</Routes>
+					</div>
 				</Router>
 			</div>
 		</IconsImports>
---
+++ src/pages/PostsPage/PostsPage.jsx
@@ -0,0 +1,17 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const PostsPage = () => {
+	return (
+		<section id="posts-page">
+			<header className="mb-5">
+				<h1>
+					<FontAwesomeIcon icon="newspaper" className="me-2" />
+					Posts
+				</h1>
+				<hr />
+			</header>
+		</section>
+	);
+};
+
+export default PostsPage;
