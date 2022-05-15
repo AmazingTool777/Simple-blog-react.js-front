d5d6e759b943fb9cc30eefd6cbd1d6510e86fee0
Tolotra <tolotor@yahoo.fr>
2022-05-15 21:14:43 +0300

    className prop to the category bullet component

--- src/pages/PostsPage/CategoryBullet/CategoryBullet.jsx
+++ src/pages/PostsPage/CategoryBullet/CategoryBullet.jsx
@@ -1,17 +1,18 @@
 // Styles
 import "./CategoryBullet.css";
 
-const CategoryBullet = ({ isDisabled = false, isActive = false, category, onClick = () => {} }) => {
+const CategoryBullet = ({ className = "", isDisabled = false, isActive = false, category, onClick = () => {} }) => {
   const handleClick = () => {
     if (isDisabled) return;
     onClick(category._id);
   };
 
   const suffix = isActive ? "primary" : "secondary";
-  let className = `category-bullet border border-1 border-${suffix} text-${suffix} px-3 py-2`;
-  if (isDisabled) className += " disabled";
+  let _className = `category-bullet border border-1 border-${suffix} text-${suffix} px-3 py-2`;
+  if (isDisabled) _className += " disabled";
+  if (className) _className += ` ${className}`;
   return (
-    <span className={className} onClick={handleClick}>
+    <span className={_className} onClick={handleClick}>
       {category.label}
     </span>
   );
