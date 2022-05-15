7fb9f07bb26d3eac5de1bd7a57256e75e4554c95
Tolotra <tolotor@yahoo.fr>
2022-05-15 14:51:05 +0300

    Disabled and onClick props on category bullet component

--- src/pages/PostsPage/CategoryBullet/CategoryBullet.jsx
+++ src/pages/PostsPage/CategoryBullet/CategoryBullet.jsx
@@ -1,10 +1,20 @@
 // Styles
 import "./CategoryBullet.css";
 
-const CategoryBullet = ({ isActive = false, category }) => {
-	const suffix = isActive ? "primary" : "secondary";
-	const className = `category-bullet border border-1 border-${suffix} text-${suffix} px-3 py-2`;
-	return <span className={className}>{category.label}</span>;
+const CategoryBullet = ({ isDisabled = false, isActive = false, category, onClick = () => {} }) => {
+  const handleClick = () => {
+    if (isDisabled) return;
+    onClick(category._id);
+  };
+
+  const suffix = isActive ? "primary" : "secondary";
+  let className = `category-bullet border border-1 border-${suffix} text-${suffix} px-3 py-2`;
+  if (isDisabled) className += " disabled";
+  return (
+    <span className={className} onClick={handleClick}>
+      {category.label}
+    </span>
+  );
 };
 
 export default CategoryBullet;
