069f38578a9371978df6e0420296c202dcfd31c8
Tolotra Rabefaly <tolotor@yahoo.fr>
2022-05-16 18:44:50 +0300

    usePost hook with post fetch effect

---
+++ src/pages/PostPage/usePost.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+
+// API calls
+import { fetchPost } from "../../apis/posts-api";
+
+function usePost(postId) {
+	const [post, setPost] = useState(null);
+	const [isLoading, setLoading] = useState(false);
+
+	useEffect(() => {
+		fetchPost(postId)
+			.then((post) => {
+				console.log(post);
+				setLoading(false);
+				setPost(post);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}, [postId]);
+
+	return { post, isLoading };
+}
+
+export default usePost;
