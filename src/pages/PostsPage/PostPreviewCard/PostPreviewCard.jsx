401ba7de3dd909dfd255e1f52316b4f63320ba0f
Tolotra <tolotor@yahoo.fr>
2022-05-16 12:37:57 +0300

    Post preview card component

---
+++ src/pages/PostsPage/PostPreviewCard/PostPreviewCard.jsx
@@ -0,0 +1,41 @@
+import { Link, useNavigate } from "react-router-dom";
+import Card from "react-bootstrap/Card";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { getDateISO, getTimeISO } from "../../../utils/dates-utils";
+
+// Styles
+import "./PostsPreviewCard.css";
+
+const PostsPreviewCard = ({ post }) => {
+  const navigate = useNavigate();
+
+  return (
+    <article>
+      <Card onClick={() => navigate(`/posts/${post._id}`)} className="post-preview-card mb-4">
+        <Card.Img variant="top" src={post.photoURL} className="img-center" />
+        <Card.Body>
+          <Card.Title className="mb-3">{post.title}</Card.Title>
+          <Card.Text className="text-primary mb-2">
+            <FontAwesomeIcon icon="user" className="me-2" />
+            <span className="text-secondary fst-italic">by</span>{" "}
+            <Link
+              to={`/users/${post.author._id}`}
+              className="fw-bolder text-decoration-none"
+              onClick={(e) => e.stopPropagation()}
+            >
+              {post.author.firstName} {post.author.lastName}
+            </Link>
+          </Card.Text>
+          <Card.Text className="fs-6 text-secondary">
+            <FontAwesomeIcon icon="clock" className="me-2" />
+            <em className="fst-normal fw-bolder">{getDateISO(post.createdAt)}</em>
+            <span className="text-dark fst-italic"> at </span>
+            <em className="fst-normal fw-bolder">{getTimeISO(post.createdAt)}</em>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </article>
+  );
+};
+
+export default PostsPreviewCard;
---
+++ src/pages/PostsPage/PostPreviewCard/index.jsx
@@ -0,0 +1,3 @@
+import PostsPreviewCard from "./PostPreviewCard";
+
+export default PostsPreviewCard;
