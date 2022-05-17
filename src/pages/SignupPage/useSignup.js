01fd279d9e8c61882d3bee4c5f8d24f66c9c1a50
Tolotra <tolotor@yahoo.fr>
2022-05-17 20:38:04 +0300

    Fields change handler and schema validation in useSignup custom hook

--- src/pages/SignupPage/useSignup.js
+++ src/pages/SignupPage/useSignup.js
@@ -1,10 +1,36 @@
 import { useState } from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+// Initial fields values
+const initialValues = {
+  firstName: "",
+  lastName: "",
+  gender: "",
+  email: "",
+  password: "",
+  passwordConfirmation: "",
+};
+
+// Validation schema for the fields' values
+const validationSchema = Yup.object({
+  firstName: Yup.string().required("This field is required").min(2, "Must contain at least 2 characters"),
+  lastName: Yup.string().required("This field is required").min(2, "Must contain at least 2 characters"),
+  gender: Yup.string().required("Gender is required"),
+  email: Yup.string().required("This field is required").email("Invalid email format"),
+  password: Yup.string().required("This field is required").min(5, "Must contain at least 5 characters"),
+  passwordConfirmation: Yup.string()
+    .required("This field is required")
+    .oneOf([Yup.ref("password")], "Incorrect password confirmation"),
+});
 
 // Custom hook for the signup page
 function useSignup() {
   const [step, setStep] = useState(1);
 
-  return { step, handleStepChange: setStep };
+  const { values, errors, touched, handleChange } = useFormik({ initialValues, validationSchema });
+
+  return { step, values, touched, errors, handleChange, handleStepChange: setStep };
 }
 
 export default useSignup;
