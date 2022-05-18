98b9a6f21702fec43f3b649df2cde1f9cec8cc07
Tolotra Rabefaly <tolotor@yahoo.fr>
2022-05-18 20:54:50 +0300

    Current user provider component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-d
 import "bootstrap/dist/css/bootstrap.min.css";
 
 // Components
+import CurrentUserProvider from "./components/CurrentUserProvider";
 import IconsImports from "./components/IconsImports";
 import AppNavbar from "./components/AppNavbar";
 import MainContentLayout from "./components/MainContentLayout";
@@ -14,27 +15,29 @@ import SignupPage from "./pages/SignupPage";
 import "./App.css";
 
 function App() {
-  return (
-    <IconsImports>
-      <div className="App">
-        <Router>
-          <AppNavbar />
-          <div id="pages-wrapper">
-            <Routes>
-              <Route path="/" element={<MainContentLayout />}>
-                <Route index element={<Navigate to="/posts" replace />} />
-                <Route path="/posts" element={<PostsPage />} />
-                <Route path="/posts/:postId" element={<PostPage />} />
-              </Route>
-              <Route path="/auth/*" element={<AuthPagesLayout />}>
-                <Route path="signup" element={<SignupPage />} />
-              </Route>
-            </Routes>
-          </div>
-        </Router>
-      </div>
-    </IconsImports>
-  );
+	return (
+		<div className="App">
+			<IconsImports>
+				<CurrentUserProvider>
+					<Router>
+						<AppNavbar />
+						<div id="pages-wrapper">
+							<Routes>
+								<Route path="/" element={<MainContentLayout />}>
+									<Route index element={<Navigate to="/posts" replace />} />
+									<Route path="/posts" element={<PostsPage />} />
+									<Route path="/posts/:postId" element={<PostPage />} />
+								</Route>
+								<Route path="/auth/*" element={<AuthPagesLayout />}>
+									<Route path="signup" element={<SignupPage />} />
+								</Route>
+							</Routes>
+						</div>
+					</Router>
+				</CurrentUserProvider>
+			</IconsImports>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/CurrentUserProvider/CurrentUserProvider.jsx
@@ -0,0 +1,55 @@
+import { useState, useEffect, useMemo, useCallback } from "react";
+
+// The current user context
+import currentUserContext from "../../contexts/currentUser-context";
+
+// API calls
+import { authenticateUserFromToken } from "../../apis/users-api";
+
+const CurrentUserProvider = ({ children }) => {
+	const [currentUser, setCurrentUser] = useState(null);
+	const [initialSetupIsDone, setInitialSetupState] = useState(false);
+
+	// Whether the current user is logged in or not
+	const isLoggedIn = useMemo(() => initialSetupIsDone && currentUser !== null, [initialSetupIsDone, currentUser]);
+
+	// Logs out the current user
+	const logout = useCallback((callback) => {
+		// Clearing the tokens
+		localStorage.removeItem("access-token");
+		setCurrentUser(null);
+		setInitialSetupState(true);
+		if (callback) callback();
+	}, []);
+
+	const contextValue = useMemo(
+		() => ({
+			currentUser,
+			initialSetupIsDone,
+			isLoggedIn,
+			setCurrentUser,
+			logout,
+		}),
+		[currentUser, initialSetupIsDone, isLoggedIn, logout]
+	);
+
+	/* Attempting to authenticate the user from the eventual access token stored in local storage */
+	useEffect(() => {
+		if (localStorage.getItem("access-token")) {
+			authenticateUserFromToken()
+				.then((user) => {
+					console.log(user);
+					setCurrentUser(user);
+					setInitialSetupState(true);
+				})
+				.catch((error) => {
+					console.log(error);
+					logout();
+				});
+		} else setInitialSetupState(true);
+	}, [logout]);
+
+	return <currentUserContext.Provider value={contextValue}>{children}</currentUserContext.Provider>;
+};
+
+export default CurrentUserProvider;
