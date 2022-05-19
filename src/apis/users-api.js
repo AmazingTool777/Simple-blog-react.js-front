356e08b988977618bb51fc91c97ac906089d6343
Tolotra <tolotor@yahoo.fr>
2022-05-19 15:54:21 +0300

    User login api call

--- src/apis/users-api.js
+++ src/apis/users-api.js
@@ -3,17 +3,25 @@ import apiConfig from "../configs/api-config";
 
 // Signs up a user
 async function signupUser(signupData) {
-	const ENDPOINT = `${apiConfig.URL}/signup`;
-	const response = (await axios.post(ENDPOINT, signupData)).data;
-	localStorage.setItem("access-token", response.token); // Storing the tokens
-	return response.user;
+  const ENDPOINT = `${apiConfig.URL}/signup`;
+  const response = (await axios.post(ENDPOINT, signupData)).data;
+  localStorage.setItem("access-token", response.token); // Storing the tokens
+  return response.user;
 }
 
 // Authenticates a user from the access-token
 async function authenticateUserFromToken() {
-	const ENDPOINT = `${apiConfig.URL}/tokenUser`;
-	const token = localStorage.getItem("access-token");
-	return (await axios.get(ENDPOINT, { headers: { Authorization: `Bearer ${token}` } })).data;
+  const ENDPOINT = `${apiConfig.URL}/tokenUser`;
+  const token = localStorage.getItem("access-token");
+  return (await axios.get(ENDPOINT, { headers: { Authorization: `Bearer ${token}` } })).data;
 }
 
-export { signupUser, authenticateUserFromToken };
+// Logs in a user
+async function loginUser(credentials) {
+  const ENDPOINT = `${apiConfig.URL}/login`;
+  const response = (await axios.post(ENDPOINT, credentials)).data;
+  localStorage.setItem("access-token", response.token); // Storing the tokens
+  return response.user;
+}
+
+export { signupUser, authenticateUserFromToken, loginUser };
