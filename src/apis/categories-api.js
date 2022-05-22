f8afb7663073ed96b4e33de11a0112594a60f0ef
Tolotra <tolotor@yahoo.fr>
2022-05-22 17:37:21 +0300

    Remove log in categories api call

--- src/apis/categories-api.js
+++ src/apis/categories-api.js
@@ -5,11 +5,10 @@ const ENDPOINT = `${apiConfig.URL}/categories`;
 
 // Fetches paginated categories
 async function fetchPaginatedCategories(page = 1, limit = 12, search = "") {
-	const response = await axios.get(ENDPOINT, {
-		params: { page, limit, search: encodeURI(search) },
-	});
-	console.log(response.data);
-	return response.data;
+  const response = await axios.get(ENDPOINT, {
+    params: { page, limit, search: encodeURI(search) },
+  });
+  return response.data;
 }
 
 export { fetchPaginatedCategories };
