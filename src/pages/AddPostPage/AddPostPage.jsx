42d30f48de553e305685a0e63cee8d25c22f348a
Tolotra <tolotor@yahoo.fr>
2022-05-22 17:36:20 +0300

    Add post page integartion with custom hook

--- src/pages/AddPostPage/AddPostPage.jsx
+++ src/pages/AddPostPage/AddPostPage.jsx
@@ -1,4 +1,4 @@
-// import { useState, useCallback } from "react";
+import { useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
@@ -7,63 +7,112 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // Styles
 import "./AddPostPage.css";
 
+// Custom hooks
+import useAddPost from "./useAddPost";
+
 // Components
 import PhotoPreview from "../../components/PhotoPreview";
 import CategoriesFields from "./CategoriesFields";
 
 const AddPostPage = () => {
-	const navigate = useNavigate();
+  const photoRef = useRef(null);
 
-	return (
-		<section id="add-post-page">
-			<div className="container pb-5">
-				<Button type="button" variant="light" className="mt-3" onClick={() => navigate(-1)}>
-					<FontAwesomeIcon icon="arrow-left" className="me-2" />
-					Go back
-				</Button>
-				<hr />
-				<h1 className="mt-4 mb-5">
-					<FontAwesomeIcon icon="plus" className="me-3" />
-					Create a new post
-				</h1>
-				<div className="form-container">
-					<Form id="add-post-form">
-						<Form.Group controlId="post-title" className="post-title-group mb-4">
-							<Form.Label>Title</Form.Label>
-							<Form.Control type="text" placeholder="Title of the post" autoFocus />
-						</Form.Group>
-						<Form.Group controlId="post-photo" className="post-photo-group mb-4">
-							<Form.Label>Photo</Form.Label>
-							<PhotoPreview isOpen={true}>
-								<div className="post-photo-options mb-2">
-									<PhotoPreview.Trigger as={Button} variant="light" className="me-3">
-										<FontAwesomeIcon icon="file-image" className="me-2" />
-										Pick a photo
-									</PhotoPreview.Trigger>
-								</div>
-								<PhotoPreview.Preview />
-							</PhotoPreview>
-						</Form.Group>
-						<div className="post-content-categories-actions-group">
-							<Form.Group controlId="post-content" className="mb-4">
-								<Form.Label>Content</Form.Label>
-								<Form.Control as="textarea" placeholder="Content of te post" rows={5} />
-							</Form.Group>
-							<CategoriesFields />
-							<div className="mt-5">
-								<Button type="submit" variant="primary" className="me-3">
-									Create
-								</Button>
-								<Button type="reset" variant="outline-secondary">
-									Reset
-								</Button>
-							</div>
-						</div>
-					</Form>
-				</div>
-			</div>
-		</section>
-	);
+  const {
+    values,
+    touched,
+    errors,
+    handleChange,
+    handlePhotoChange,
+    handleCategoriesChange,
+    handleNewCategoryAdd,
+    handleNewCategoryDelete,
+    handleBlur,
+    handleSubmit,
+  } = useAddPost();
+
+  const navigate = useNavigate();
+
+  return (
+    <section id="add-post-page">
+      <div className="container pb-5">
+        <Button type="button" variant="light" className="mt-3" onClick={() => navigate(-1)}>
+          <FontAwesomeIcon icon="arrow-left" className="me-2" />
+          Go back
+        </Button>
+        <hr />
+        <h1 className="mt-4 mb-5">
+          <FontAwesomeIcon icon="plus" className="me-3" />
+          Create a new post
+        </h1>
+        <div className="form-container">
+          <Form id="add-post-form" onSubmit={handleSubmit}>
+            <Form.Group controlId="post-title" className="post-title-group mb-4">
+              <Form.Label>Title</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Title of the post"
+                name="title"
+                value={values.title}
+                isInvalid={!!errors.title && touched.title}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+              <Form.Control.Feedback type="invalid">{errors.title}</Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group controlId="post-photo" className="post-photo-group mb-4">
+              <Form.Label>Photo</Form.Label>
+              <PhotoPreview ref={photoRef} name="photo" isOpen={true} onFileChange={handlePhotoChange}>
+                <div className="post-photo-options mb-2">
+                  <PhotoPreview.Trigger as={Button} variant="light" className="me-3">
+                    <FontAwesomeIcon icon="file-image" className="me-2" />
+                    Pick a photo
+                  </PhotoPreview.Trigger>
+                </div>
+                <Form.Control type="hidden" isInvalid={!!errors.photo && touched.photo} />
+                <Form.Control.Feedback type="invalid" className="mb-2">
+                  {errors.photo}
+                </Form.Control.Feedback>
+                <PhotoPreview.Preview className="post-photo-preview" />
+              </PhotoPreview>
+            </Form.Group>
+            <div className="post-content-categories-actions-group">
+              <Form.Group controlId="post-content" className="mb-4">
+                <Form.Label>Content</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  rows={5}
+                  placeholder="Content of the post"
+                  name="content"
+                  value={values.content}
+                  isInvalid={!!errors.content && touched.content}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                <Form.Control.Feedback type="invalid">{errors.content}</Form.Control.Feedback>
+              </Form.Group>
+              <CategoriesFields
+                newCategories={values.newCategories}
+                error={errors.categories}
+                isTouched={touched.categories}
+                onSelectChange={handleCategoriesChange}
+                onNewCategoryAdd={handleNewCategoryAdd}
+                onNewCategoryDelete={handleNewCategoryDelete}
+                onBlur={handleBlur}
+              />
+              <div className="mt-5">
+                <Button type="submit" variant="primary" className="me-3">
+                  Create
+                </Button>
+                <Button type="reset" variant="outline-secondary">
+                  Reset
+                </Button>
+              </div>
+            </div>
+          </Form>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default AddPostPage;
