a87dfc7fb5fd9ff3ee779a944fc365bec3331c98
Tolotra <tolotor@yahoo.fr>
2022-05-22 22:26:57 +0300

    API call for adding a new post

--- src/apis/posts-api.js
+++ src/apis/posts-api.js
@@ -5,18 +5,34 @@ const ENDPOINT = `${apiConfig.URL}/posts`;
 
 // Fetches paginated posts
 async function fetchPaginatedPosts(page = 1, limit = 12, order = "desc", search = "") {
-	return axios
-		.get(ENDPOINT, {
-			params: { page, limit, order, search: encodeURI(search) },
-		})
-		.then((response) => response.data);
+  return axios
+    .get(ENDPOINT, {
+      params: { page, limit, order, search: encodeURI(search) },
+    })
+    .then((response) => response.data);
 }
 
 // Fetches a post by id
 async function fetchPost(id) {
-	const URL = `${ENDPOINT}/${id}`;
-	const result = await axios.get(URL);
-	return result.data;
+  const URL = `${ENDPOINT}/${id}`;
+  const result = await axios.get(URL);
+  return result.data;
 }
 
-export { fetchPaginatedPosts, fetchPost };
+// Adds a new post to api
+async function addPost(postData, onUploadProgress) {
+  const token = localStorage.getItem("access-token");
+  const response = await axios.post(ENDPOINT, postData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+      Authorization: `Bearer ${token}`,
+    },
+    onUploadProgress: (e) => {
+      const progress = parseInt(Math.round((e.loaded * 100) / e.total));
+      onUploadProgress(progress);
+    },
+  });
+  return response.data;
+}
+
+export { fetchPaginatedPosts, fetchPost, addPost };
