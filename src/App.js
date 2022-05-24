97355281d739409ebb4b13e663fc35dd16ac8afc
Tolotra <tolotor@yahoo.fr>
2022-05-24 12:03:19 +0300

    Toasts provider in App

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import currentUserContext from "./contexts/currentUser-context";
 // Components
 import AppPreloader from "./components/AppPreloader";
 import CurrentUserProvider from "./components/CurrentUserProvider";
+import ToastsProvider from "./components/ToastsProvider";
 import ProtectedRoute from "./components/ProtectedRoute";
 import IconsImports from "./components/IconsImports";
 import AppNavbar from "./components/AppNavbar";
@@ -30,42 +31,44 @@ function App() {
           !initialSetupIsDone ? (
             <AppPreloader />
           ) : (
-            <IconsImports>
-              <Router>
-                <div className="App">
-                  <AppNavbar />
-                  <div id="pages-wrapper">
-                    <Routes>
-                      <Route path="/" element={<MainContentLayout />}>
-                        <Route index element={<Navigate to="/posts" replace />} />
-                        <Route path="/posts" element={<PostsPage />} />
-                        <Route path="/posts/:postId" element={<PostPage />} />
-                      </Route>
-                      <Route
-                        path="/auth/*"
-                        element={
-                          <ProtectedRoute reverse={true}>
-                            <AuthPagesLayout />
-                          </ProtectedRoute>
-                        }
-                      >
-                        <Route path="signup" element={<SignupPage />} />
-                        <Route path="login" element={<SigninPage />} />
-                      </Route>
-                      <Route
-                        path="/add-post"
-                        element={
-                          <ProtectedRoute>
-                            <AddPostPage />
-                          </ProtectedRoute>
-                        }
-                      />
-                      <Route path="/test" element={<TestPage />} />
-                    </Routes>
+            <ToastsProvider>
+              <IconsImports>
+                <Router>
+                  <div className="App">
+                    <AppNavbar />
+                    <div id="pages-wrapper">
+                      <Routes>
+                        <Route path="/" element={<MainContentLayout />}>
+                          <Route index element={<Navigate to="/posts" replace />} />
+                          <Route path="/posts" element={<PostsPage />} />
+                          <Route path="/posts/:postId" element={<PostPage />} />
+                        </Route>
+                        <Route
+                          path="/auth/*"
+                          element={
+                            <ProtectedRoute reverse={true}>
+                              <AuthPagesLayout />
+                            </ProtectedRoute>
+                          }
+                        >
+                          <Route path="signup" element={<SignupPage />} />
+                          <Route path="login" element={<SigninPage />} />
+                        </Route>
+                        <Route
+                          path="/add-post"
+                          element={
+                            <ProtectedRoute>
+                              <AddPostPage />
+                            </ProtectedRoute>
+                          }
+                        />
+                        <Route path="/test" element={<TestPage />} />
+                      </Routes>
+                    </div>
                   </div>
-                </div>
-              </Router>
-            </IconsImports>
+                </Router>
+              </IconsImports>
+            </ToastsProvider>
           )
         }
       </currentUserContext.Consumer>
