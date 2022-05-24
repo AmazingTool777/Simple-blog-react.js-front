9dff348fd38b27c7b6b5b3868f18997caa479e34
Tolotra <tolotor@yahoo.fr>
2022-05-24 09:11:07 +0300

    Fix in useCheckIfUpdated custom hook

--- src/hooks/useCheckIfUpdated.js
+++ src/hooks/useCheckIfUpdated.js
@@ -3,7 +3,7 @@ import { useRef } from "react";
 /* Custom hook for checking if a component has been updated on not */
 function useCheckIfUpdated() {
   const updatesNbRef = useRef(0);
-  if (updatesNbRef.current === 1) updatesNbRef.current++;
+  if (updatesNbRef.current <= 1) updatesNbRef.current++;
   return updatesNbRef.current > 1;
 }
 
