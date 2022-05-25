b51db0fbb25075e416419052cf8caca47638b9ae
Tolotra Rabefaly <tolotor@yahoo.fr>
2022-05-25 17:41:12 +0300

    User card component initial setup

---
+++ src/pages/UsersPage/UserCard/UserCard.jsx
@@ -0,0 +1,8 @@
+// Styles
+import "./UserCard.css";
+
+const UserCard = () => {
+	return <article className="user-card"></article>;
+};
+
+export default UserCard;
--- src/pages/UsersPage/UsersPage.jsx
+++ src/pages/UsersPage/UsersPage.jsx
@@ -6,9 +6,10 @@ import useUsers from "./useUsers";
 
 // Components
 import UsersParams from "./UsersParams";
+import UserCard from "./UserCard";
 
 const UsersPage = () => {
-	const { users, order, isLoading, handleOrderChange, handleSearchChange } = useUsers();
+	const { users, order, isLoading, count, handleOrderChange, handleSearchChange } = useUsers();
 
 	useEffect(() => {
 		console.log(users);
@@ -29,6 +30,13 @@ const UsersPage = () => {
 				onOrderChange={handleOrderChange}
 				onSearchSubmit={handleSearchChange}
 			/>
+			<p className="text-secondary">
+				<strong className="text-dark">{count}</strong> result{count > 1 && "s"}
+			</p>
+			<hr />
+			<div className="users-list">
+				<UserCard />
+			</div>
 		</section>
 	);
 };
