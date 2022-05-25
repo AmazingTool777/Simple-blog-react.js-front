9cb753fb2f44417aa597b5abab92dfdebf7a2878
Tolotra Rabefaly <tolotor@yahoo.fr>
2022-05-25 15:46:11 +0300

    useUsers custom hook with fetch logic

--- src/pages/UsersPage/useUsers.js
+++ src/pages/UsersPage/useUsers.js
@@ -1,4 +1,36 @@
+import { useState } from "react";
+
+// API calls
+import { fetchUsers } from "../../apis/users-api";
+
+// Other custom hooks
+import usePaginatedApiCall from "../../hooks/usePaginatedApiCall";
+
 // Custom hook for the users page
 export default function useUsers() {
-	return {};
+	const [page, setPage] = useState(1);
+	const [order, setOrder] = useState("desc");
+	const [search, setSearch] = useState("");
+
+	const [users, isLoading, count, pages] = usePaginatedApiCall(
+		() => {
+			const LIMIT = 12;
+			return fetchUsers(page, LIMIT, order, search);
+		},
+		null,
+		[page, order, search]
+	);
+
+	return {
+		users,
+		page,
+		order,
+		search,
+		isLoading,
+		count,
+		pages,
+		handlePageChange: setPage,
+		handleOrderChange: setOrder,
+		handleSearchChange: setSearch,
+	};
 }
