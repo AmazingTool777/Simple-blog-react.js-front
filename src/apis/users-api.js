4b3899c7b9893fbf98b36cb468e9ad285f910339
Tolotra Rabefaly <tolotor@yahoo.fr>
2022-05-25 15:19:21 +0300

    Fetch users api call

--- src/apis/users-api.js
+++ src/apis/users-api.js
@@ -1,27 +1,35 @@
 import axios from "axios";
 import apiConfig from "../configs/api-config";
 
+const ENDPOINT = `${apiConfig.URL}/users`;
+
 // Signs up a user
 async function signupUser(signupData) {
-  const ENDPOINT = `${apiConfig.URL}/signup`;
-  const response = (await axios.post(ENDPOINT, signupData)).data;
-  localStorage.setItem("access-token", response.token); // Storing the tokens
-  return response.user;
+	const ENDPOINT = `${apiConfig.URL}/signup`;
+	const response = (await axios.post(ENDPOINT, signupData)).data;
+	localStorage.setItem("access-token", response.token); // Storing the tokens
+	return response.user;
 }
 
 // Authenticates a user from the access-token
 async function authenticateUserFromToken() {
-  const ENDPOINT = `${apiConfig.URL}/tokenUser`;
-  const token = localStorage.getItem("access-token");
-  return (await axios.get(ENDPOINT, { headers: { Authorization: `Bearer ${token}` } })).data;
+	const ENDPOINT = `${apiConfig.URL}/tokenUser`;
+	const token = localStorage.getItem("access-token");
+	return (await axios.get(ENDPOINT, { headers: { Authorization: `Bearer ${token}` } })).data;
 }
 
 // Logs in a user
 async function loginUser(credentials) {
-  const ENDPOINT = `${apiConfig.URL}/login`;
-  const response = (await axios.post(ENDPOINT, credentials)).data;
-  localStorage.setItem("access-token", response.token); // Storing the tokens
-  return response.user;
+	const ENDPOINT = `${apiConfig.URL}/login`;
+	const response = (await axios.post(ENDPOINT, credentials)).data;
+	localStorage.setItem("access-token", response.token); // Storing the tokens
+	return response.user;
 }
 
-export { signupUser, authenticateUserFromToken, loginUser };
+// Gets users
+async function fetchUsers(page = 1, limit = 12, order = "DESC", search = "") {
+	const url = `${ENDPOINT}?page=${page}&limit=${limit}&order=${order}&search=${search}`;
+	return (await axios.get(url)).data;
+}
+
+export { signupUser, authenticateUserFromToken, loginUser, fetchUsers };
