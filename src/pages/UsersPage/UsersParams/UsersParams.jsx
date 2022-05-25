816a205727f74201a8c74aac80eb6cad75051082
Tolotra Rabefaly <tolotor@yahoo.fr>
2022-05-25 21:27:03 +0300

    Users params reponsive layout

--- src/pages/UsersPage/UsersParams/UsersParams.jsx
+++ src/pages/UsersPage/UsersParams/UsersParams.jsx
@@ -17,8 +17,23 @@ const UsersParams = ({ isDisabled = false, order, onOrderChange, onSearchSubmit 
 	const handleOrderChange = useCallback((e) => onOrderChange(e.target.value), [onOrderChange]);
 
 	return (
-		<div className="d-flex flex-column-reverse mb-5">
-			<div className="users-filter">
+		<div className="d-flex flex-column flex-md-row-reverse justify-content-between align-items-md-center mb-5">
+			<Form className="users-search mb-3 mb-md-0" onSubmit={handleSearchSubmit}>
+				<div className="d-flex align-items-center">
+					<Form.Control
+						type="search"
+						placeholder="Search users"
+						className="me-2"
+						disabled={isDisabled}
+						value={search}
+						onChange={(e) => setSearch(e.target.value)}
+					/>
+					<Button type="submit" disabled={isDisabled}>
+						<FontAwesomeIcon icon="search" />
+					</Button>
+				</div>
+			</Form>
+			<div className="users-filter text-center text-md-left">
 				<FontAwesomeIcon icon="clock" className="me-2" />
 				<em className="fst-normal fw-bolder text-secondary me-3">Sort:</em>
 				<Form.Check
@@ -44,21 +59,6 @@ const UsersParams = ({ isDisabled = false, order, onOrderChange, onSearchSubmit 
 					onChange={handleOrderChange}
 				/>
 			</div>
-			<Form className="users-search mb-3" onSubmit={handleSearchSubmit}>
-				<div className="d-flex align-items-center">
-					<Form.Control
-						type="search"
-						placeholder="Search users"
-						className="me-2"
-						disabled={isDisabled}
-						value={search}
-						onChange={(e) => setSearch(e.target.value)}
-					/>
-					<Button type="submit" disabled={isDisabled}>
-						<FontAwesomeIcon icon="search" />
-					</Button>
-				</div>
-			</Form>
 		</div>
 	);
 };
