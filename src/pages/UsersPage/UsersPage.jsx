405221013874ed93b43bdb68f4430332b686faee
Tolotra Rabefaly <tolotor@yahoo.fr>
2022-05-26 11:07:32 +0300

    Page increment effect on end of scroll in users page

--- src/pages/UsersPage/UsersPage.jsx
+++ src/pages/UsersPage/UsersPage.jsx
@@ -1,21 +1,26 @@
-import { useEffect } from "react";
+import { useCallback } from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 // Custom hooks
 import useUsers from "./useUsers";
+import useScrollEndObserver from "../../hooks/useScrollEndObserver";
 
 // Components
 import UsersParams from "./UsersParams";
 import UserCard, { UserCardLoaders } from "./UserCard";
+import { ResultsNumberLoader } from "../../components/Loaders";
 
 const UsersPage = () => {
-	const { users, order, page, pages, isLoading, count, handleOrderChange, handleSearchChange } = useUsers();
+	const { users, order, page, pages, isLoading, count, handleOrderChange, handlePageChange, handleSearchChange } =
+		useUsers();
 
-	useEffect(() => {
-		console.log(users);
-	}, [users]);
+	const handleEndOfScroll = useCallback(() => {
+		if (!isLoading && page < pages) handlePageChange(page + 1);
+	}, [page, pages, isLoading, handlePageChange]);
+
+	useScrollEndObserver(handleEndOfScroll);
 
 	return (
 		<section id="users-page">
@@ -33,32 +38,40 @@ const UsersPage = () => {
 				onSearchSubmit={handleSearchChange}
 			/>
 			<p className="text-secondary">
-				<strong className="text-dark">{count}</strong> result{count > 1 && "s"}
-			</p>
-			<hr />
-			<Row className="mt-4">
-				{(page !== 1 || !isLoading) &&
-					users.map((user) => (
-						<Col key={user._id} xs={12} md={6} lg={4}>
-							<UserCard user={user} />
-						</Col>
-					))}
-				{isLoading && (
+				{page === 1 && isLoading ? (
+					<ResultsNumberLoader />
+				) : (
 					<>
-						<Col xs={12} md={6} lg={4}>
-							<UserCardLoaders />
-						</Col>
-						<Col xs={12} md={6} lg={4}>
-							<UserCardLoaders />
-						</Col>
+						<strong className="text-dark">{count}</strong> result{count > 1 && "s"}
 					</>
 				)}
-				{!isLoading && page === pages && (
-					<Col xs={12}>
-						<p className="text-center text-secondary">End of results.</p>
-					</Col>
-				)}
-			</Row>
+			</p>
+			<hr />
+			<div className="users-list">
+				<Row className="mt-4">
+					{(page !== 1 || !isLoading) &&
+						users.map((user) => (
+							<Col key={user._id} xs={12} md={6} lg={4}>
+								<UserCard user={user} />
+							</Col>
+						))}
+					{isLoading && (
+						<>
+							<Col xs={12} md={6} lg={4}>
+								<UserCardLoaders />
+							</Col>
+							<Col xs={12} md={6} lg={4}>
+								<UserCardLoaders />
+							</Col>
+						</>
+					)}
+					{!isLoading && page === pages && (
+						<Col xs={12}>
+							<p className="text-center text-secondary">End of results.</p>
+						</Col>
+					)}
+				</Row>
+			</div>
 		</section>
 	);
 };
