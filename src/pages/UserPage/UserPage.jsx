b2af37f57b30c8f25f7dbd2a26422d83390bb2ea
Tolotra Rabefaly <tolotor@yahoo.fr>
2022-05-27 21:27:13 +0300

    User page component with hook integration

--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -1,5 +1,67 @@
+import Badge from "react-bootstrap/Badge";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+// Styles
+import "./UserPage.css";
+
+// Utils
+import { getDateISO } from "../../utils/dates-utils";
+
+// Custom hooks
+import useUser from "./useUser";
+
+// Components
+import AppBreadcrumbNav from "../../components/AppBreadcrumbNav";
+import UserAvatar from "../../components/UserAvatar";
+import UserPageLoaders from "./UserPageLoaders";
+
 const UserPage = () => {
-	return <section id="user-page">Hello</section>;
+	const { user, isLoading } = useUser();
+
+	const fullName = user ? `${user.firstName} ${user.lastName}` : null;
+
+	return (
+		<section id="user-page">
+			<AppBreadcrumbNav
+				isLoading={isLoading}
+				basePath="/users"
+				baseEntity="Users"
+				title={user ? fullName : null}
+				notFoundMessage="User not found"
+			/>
+			{isLoading ? (
+				<UserPageLoaders />
+			) : (
+				<>
+					{user && (
+						<article className="mt-5">
+							<div className="profile-layout d-flex align-items-start">
+								<UserAvatar
+									className="profile-avatar"
+									src={user.photoURL}
+									alt={`${fullName}'s profile photo`}
+									gender={user.gender}
+								/>
+								<div className="profile-info flex-fill ms-3 ms-md-4">
+									<h1 className="user-name fs-3 mb-3">{fullName}</h1>
+									<p className="user-email mb-2">
+										<FontAwesomeIcon icon="envelope" className="me-2 text-secondary" />
+										<Badge as="em" bg="secondary" className="fw-bolder fst-normal">
+											{user.email}
+										</Badge>
+									</p>
+									<p className="user-join_date text-dark fst-italic">
+										<FontAwesomeIcon icon="clock" className="me-2 text-secondary" />
+										Member since <strong className="text-secondary">{getDateISO(user.createdAt)}</strong>
+									</p>
+								</div>
+							</div>
+						</article>
+					)}
+				</>
+			)}
+		</section>
+	);
 };
 
 export default UserPage;
--- src/pages/UserPage/index.jsx
+++ src/pages/UserPage/index.jsx
@@ -1,3 +1,4 @@
 import UserPage from "./UserPage";
+export { default as UserPageLoaders } from "./UserPageLoaders";
 
 export default UserPage;
