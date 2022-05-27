8771d08bcdc741507201b27001608176ecb2b8e4
Tolotra Rabefaly <tolotor@yahoo.fr>
2022-05-27 21:26:01 +0300

    User page loaders

---
+++ src/pages/UserPage/UserPageLoaders.jsx
@@ -0,0 +1,11 @@
+import { Facebook } from "react-content-loader";
+
+const UserPageLoaders = () => {
+	return (
+		<div className="mt-5">
+			<Facebook style={{ maxWidth: "800px" }} />
+		</div>
+	);
+};
+
+export default UserPageLoaders;
