f49119275c2db326e0dba9115c94dd066a50b1c5
Tolotra <tolotor@yahoo.fr>
2022-05-28 06:30:23 +0300

    Add has fetched state in user page

--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -17,53 +17,53 @@ import NotFoundFiller from "../../components/NotFoundFiller";
 import UserPageLoaders from "./UserPageLoaders";
 
 const UserPage = () => {
-	const { user, isLoading } = useUser();
+  const { user, isLoading, hasFetched } = useUser();
 
-	const fullName = user ? `${user.firstName} ${user.lastName}` : null;
+  const fullName = user ? `${user.firstName} ${user.lastName}` : null;
 
-	return (
-		<section id="user-page">
-			<AppBreadcrumbNav
-				isLoading={isLoading}
-				basePath="/users"
-				baseEntity="Users"
-				title={user ? fullName : null}
-				notFoundMessage="User not found"
-			/>
-			{isLoading ? (
-				<UserPageLoaders />
-			) : (
-				<>
-					{user && (
-						<article className="mt-5">
-							<div className="profile-layout d-flex align-items-start">
-								<UserAvatar
-									className="profile-avatar"
-									src={user.photoURL}
-									alt={`${fullName}'s profile photo`}
-									gender={user.gender}
-								/>
-								<div className="profile-info flex-fill ms-3 ms-md-4">
-									<h1 className="user-name fs-3 mb-3">{fullName}</h1>
-									<p className="user-email mb-2">
-										<FontAwesomeIcon icon="envelope" className="me-2 text-secondary" />
-										<Badge as="em" bg="secondary" className="fw-bolder fst-normal">
-											{user.email}
-										</Badge>
-									</p>
-									<p className="user-join_date text-dark fst-italic">
-										<FontAwesomeIcon icon="clock" className="me-2 text-secondary" />
-										Member since <strong className="text-secondary">{getDateISO(user.createdAt)}</strong>
-									</p>
-								</div>
-							</div>
-						</article>
-					)}
-					{!user && <NotFoundFiller />}
-				</>
-			)}
-		</section>
-	);
+  return (
+    <section id="user-page">
+      <AppBreadcrumbNav
+        isLoading={isLoading}
+        basePath="/users"
+        baseEntity="Users"
+        title={user ? fullName : null}
+        notFoundMessage="User not found"
+      />
+      {isLoading ? (
+        <UserPageLoaders />
+      ) : (
+        <>
+          {user && (
+            <article className="mt-5">
+              <div className="profile-layout d-flex align-items-start">
+                <UserAvatar
+                  className="profile-avatar"
+                  src={user.photoURL}
+                  alt={`${fullName}'s profile photo`}
+                  gender={user.gender}
+                />
+                <div className="profile-info flex-fill ms-3 ms-md-4">
+                  <h1 className="user-name fs-3 mb-3">{fullName}</h1>
+                  <p className="user-email mb-2">
+                    <FontAwesomeIcon icon="envelope" className="me-2 text-secondary" />
+                    <Badge as="em" bg="secondary" className="fw-bolder fst-normal">
+                      {user.email}
+                    </Badge>
+                  </p>
+                  <p className="user-join_date text-dark fst-italic">
+                    <FontAwesomeIcon icon="clock" className="me-2 text-secondary" />
+                    Member since <strong className="text-secondary">{getDateISO(user.createdAt)}</strong>
+                  </p>
+                </div>
+              </div>
+            </article>
+          )}
+          {!user && hasFetched && <NotFoundFiller />}
+        </>
+      )}
+    </section>
+  );
 };
 
 export default UserPage;
--- src/pages/UserPage/useUser.js
+++ src/pages/UserPage/useUser.js
@@ -10,45 +10,47 @@ import useToasts from "../../hooks/useToasts";
 
 // Custom hook for the user page
 export default function useUser() {
-	const [user, setUser] = useState(null);
-	const [isLoading, setLoading] = useState(false);
+  const [user, setUser] = useState(null);
+  const [isLoading, setLoading] = useState(false);
+  const [hasFetched, setHasFetched] = useState(false);
 
-	const { userId } = useParams();
+  const { userId } = useParams();
 
-	const { handleToastAdd } = useToasts();
+  const { handleToastAdd } = useToasts();
 
-	useEffect(() => {
-		setLoading(true);
-		fetchUser(userId)
-			.then((user) => {
-				setLoading(false);
-				setUser(user);
-			})
-			.catch((error) => {
-				setLoading(false);
-				setUser(null);
-				handleToastAdd({
-					type: "BROWSING_MESSAGE",
-					message: {
-						title: (
-							<>
-								<FontAwesomeIcon icon="times-circle" className="me-2 text-danger" />
-								{error.status < 500 ? "404 not found" : "Server error"}
-							</>
-						),
-						content: (
-							<p className="text-danger mb-0">
-								{error.status < 500
-									? "We cannot find the requested user."
-									: "A server error occured. Please try again."}
-							</p>
-						),
-						duration: 5000,
-					},
-				});
-			});
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [userId]);
+  useEffect(() => {
+    setLoading(true);
+    fetchUser(userId)
+      .then((user) => {
+        setLoading(false);
+        setHasFetched(true);
+        setUser(user);
+      })
+      .catch((error) => {
+        setLoading(false);
+        setUser(null);
+        handleToastAdd({
+          type: "BROWSING_MESSAGE",
+          message: {
+            title: (
+              <>
+                <FontAwesomeIcon icon="times-circle" className="me-2 text-danger" />
+                {error.status < 500 ? "404 not found" : "Server error"}
+              </>
+            ),
+            content: (
+              <p className="text-danger mb-0">
+                {error.status < 500
+                  ? "We cannot find the requested user."
+                  : "A server error occured. Please try again."}
+              </p>
+            ),
+            duration: 5000,
+          },
+        });
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [userId]);
 
-	return { user, isLoading };
+  return { user, isLoading, hasFetched };
 }
