f262865d0732733490eae8f9f09bd6e668f8defc
Tolotra <tolotor@yahoo.fr>
2022-06-09 15:02:34 +0300

    Profile photo section ui states

--- src/pages/MyAccountPage/MyAccountPage.jsx
+++ src/pages/MyAccountPage/MyAccountPage.jsx
@@ -1,16 +1,27 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+// Custom hooks
+import useMyAccount from "./useMyAccount";
+
 // Components
 import ProfilePhotoSection from "./ProfilePhotoSection";
+import MyAccountPageLoaders from "./MyAccountPageLoaders";
 
 const MyAccountPage = () => {
+  const { user, isLoading, hasFetched } = useMyAccount();
+
   return (
     <section id="my-account-page">
       <h1 className="mb-5">
         <FontAwesomeIcon icon="user" className="me-4" />
         My account
       </h1>
-      <ProfilePhotoSection />
+      {isLoading && <MyAccountPageLoaders />}
+      {!isLoading && hasFetched && (
+        <>
+          <ProfilePhotoSection user={user} />
+        </>
+      )}
     </section>
   );
 };
--- src/pages/MyAccountPage/ProfilePhotoSection/ProfilePhotoSection.jsx
+++ src/pages/MyAccountPage/ProfilePhotoSection/ProfilePhotoSection.jsx
@@ -1,7 +1,58 @@
-const ProfilePhotoSection = () => {
+import { useState, useRef, useCallback } from "react";
+import Button from "react-bootstrap/Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+// Styles
+import "./ProfilePhotoSection.css";
+
+// Components
+import UserAvatar from "../../../components/UserAvatar";
+import PhotoPreview from "../../../components/PhotoPreview";
+
+const ProfilePhotoSection = ({ user }) => {
+  const profilePhotoRef = useRef(null);
+
+  const [isOpen, setOpen] = useState(false);
+
+  const ALT = `${user.fisrtName} ${user.lastName}'s profile photo`;
+
+  const handleClose = useCallback(() => setOpen(false), []);
+
   return (
     <section>
-      <h2>Your profile photo</h2>
+      <h2 className="fs-3 mb-4">Profile photo</h2>
+      <PhotoPreview ref={profilePhotoRef} isOpen={isOpen} onClose={handleClose}>
+        <div className="d-flex">
+          <div id="current-user-avatar-wrapper" className="me-3">
+            {!isOpen && <UserAvatar id="current-user-avatar" src={user.photoURL} gender={user.gender} alt={ALT} />}
+            {isOpen && <PhotoPreview.Preview className="current-user-avatar-preview" src={user.photoURL} />}
+          </div>
+          <div>
+            {!isOpen && (
+              <Button type="button" variant="warning" onClick={() => setOpen(true)}>
+                <FontAwesomeIcon icon="pen-square" className="me-2" />
+                Edit profile photo
+              </Button>
+            )}
+            {isOpen && (
+              <PhotoPreview.Trigger as={Button} variant="secondary" type="button">
+                <FontAwesomeIcon icon="file-image" className="me-2" />
+                Pick a photo
+              </PhotoPreview.Trigger>
+            )}
+          </div>
+        </div>
+        {isOpen && (
+          <div className="d-flex mt-3">
+            <Button type="button" variant="primary">
+              Confirm
+            </Button>
+            <PhotoPreview.Closer as={Button} type="button" variant="outline-secondary" className="ms-3">
+              Cancel
+            </PhotoPreview.Closer>
+          </div>
+        )}
+      </PhotoPreview>
     </section>
   );
 };
