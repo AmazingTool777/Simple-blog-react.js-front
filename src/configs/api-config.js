31c0d3e045759784d451feec791ee0a8a2c2ea7e
Tolotra <tolotor@yahoo.fr>
2022-06-20 12:37:04 +0300

    Set api config to production

--- src/configs/api-config.js
+++ src/configs/api-config.js
@@ -1,17 +1,14 @@
 // API network config
 const apiConfig = {
-	host: "localhost",
-	port: 5000,
-	domain: null,
-	protocol: "http",
-	URL: "",
+  domain: "distinct-heavy-lighter.glitch.me",
+  protocol: "https",
 };
 
 // Getter for the URL
 Object.defineProperty(apiConfig, "URL", {
-	get: function () {
-		return this.protocol + "://" + (this.domain ? this.domain : `${this.host}:${this.port}`) + "/api";
-	},
+  get: function () {
+    return this.protocol + "://" + (this.domain ? this.domain : `${this.host}:${this.port}`) + "/api";
+  },
 });
 
 export default apiConfig;
