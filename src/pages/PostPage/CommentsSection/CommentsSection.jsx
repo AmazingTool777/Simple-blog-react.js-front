5b6b1e76afa896d0cfac05c60180c8322a3a5675
Tolotra <tolotor@yahoo.fr>
2022-07-10 13:33:27 +0300

    Integration of comment submission in custom hook to component

--- src/pages/PostPage/CommentsSection/CommentsSection.jsx
+++ src/pages/PostPage/CommentsSection/CommentsSection.jsx
@@ -1,28 +1,31 @@
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import Spinner from "react-bootstrap/Spinner";
 
 // Custom hooks
 import useNewComment from "./useNewComment";
 
 const CommentsSection = ({ post }) => {
-  const { content, handleContentChange, handleContentFocus } = useNewComment(post);
+  const { content, isSubmitting, handleContentChange, handleContentFocus, handleCommentSubmit } = useNewComment(post);
 
   return (
     <section id="post-comments">
-      <Form>
+      <Form onSubmit={handleCommentSubmit}>
         <Form.Group controlId="post-new-comment" className="mb-3">
           <Form.Label>Your comment:</Form.Label>
           <Form.Control
             as="textarea"
             rows="3"
             placeholder="Write your thoughts about this post"
+            disabled={isSubmitting}
             value={content}
             onChange={handleContentChange}
             onFocus={handleContentFocus}
           />
         </Form.Group>
-        <Button type="submit" variant="primary" disabled={!content}>
+        <Button type="submit" variant="primary" disabled={!content || isSubmitting}>
           Submit
+          {isSubmitting && <Spinner animation="border" variant="light" size="sm" className="ms-2" />}
         </Button>
       </Form>
     </section>
--- src/pages/PostPage/CommentsSection/useNewComment.js
+++ src/pages/PostPage/CommentsSection/useNewComment.js
@@ -1,12 +1,16 @@
 import { useState, useCallback } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 
+// API calls
+import { addPostComment } from "../../../apis/posts-api";
+
 // Other custom hooks
 import useCurrentUser from "../../../hooks/useCurrentUser";
 
 // Custom hook for the addition of a new comment
-export default function useNewComment(post) {
+export default function useNewComment(post, onCommentAdded = () => {}) {
   const [content, setContent] = useState("");
+  const [isSubmitting, setSubmitting] = useState(false);
 
   const { isLoggedIn } = useCurrentUser();
 
@@ -23,9 +27,29 @@ export default function useNewComment(post) {
       });
   }, [isLoggedIn, navigate, pathname]);
 
+  const handleCommentSubmit = useCallback(
+    async (e) => {
+      e.preventDefault();
+      const commentData = { content };
+      setSubmitting(true);
+      try {
+        const addedComment = await addPostComment(post._id, commentData);
+        setSubmitting(false);
+        setContent("");
+        onCommentAdded(addedComment);
+      } catch (error) {
+        setSubmitting(false);
+        console.log(error);
+      }
+    },
+    [content, onCommentAdded, post._id]
+  );
+
   return {
     content,
+    isSubmitting,
     handleContentChange,
     handleContentFocus,
+    handleCommentSubmit,
   };
 }
