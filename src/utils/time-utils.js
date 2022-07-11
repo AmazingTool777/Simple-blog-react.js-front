80210f74f5848548204bed215f376b2e75563334
Tolotra <tolotor@yahoo.fr>
2022-07-11 23:36:14 +0300

    Relative time getter util

---
+++ src/utils/time-utils.js
@@ -0,0 +1,43 @@
+// Milliseconds for each time unit
+const minuteMs = 60 * 1000;
+const hourMs = 60 * minuteMs;
+const dayMs = 24 * hourMs;
+const weekMs = 7 * dayMs;
+const monthMs = 4 * weekMs;
+const yearMs = 12 * monthMs;
+const farMs = yearMs * 100;
+
+// Breakpoints for the change of relative time unit
+const breakpoints = [
+  { ceiling: minuteMs, unit: "Now" },
+  { ceiling: hourMs, unit: "min" },
+  { ceiling: dayMs, unit: "h" },
+  { ceiling: weekMs, unit: "d" },
+  { ceiling: monthMs, unit: "w" },
+  { ceiling: yearMs, unit: "mo" },
+  { ceiling: farMs, unit: "y" },
+];
+
+// Helper for returning the relative time in regards to a time
+// and its timeout for the next relative time update display
+export function getRelativeTime(startDate, finishDate) {
+  const startDateMs = startDate.getTime();
+  const finishDateMs = finishDate.getTime();
+  const timeDiff = finishDateMs - startDateMs;
+
+  let relativeTime, nextUpdateTimeout;
+  breakpoints.some(({ ceiling, unit }, i) => {
+    if (timeDiff < ceiling) {
+      const prevCeiling = breakpoints[i - 1].ceiling;
+      if (unit === "Now") relativeTime = "Now";
+      else {
+        const relativeTimeAmount = Math.floor(timeDiff / prevCeiling);
+        relativeTime = `${relativeTimeAmount} ${unit}`;
+      }
+      nextUpdateTimeout = Math.ceil(startDateMs + Math.ceil(timeDiff / prevCeiling) * prevCeiling - finishDateMs);
+      return true;
+    } else return false;
+  });
+
+  return { relativeTime, nextUpdateTimeout };
+}
