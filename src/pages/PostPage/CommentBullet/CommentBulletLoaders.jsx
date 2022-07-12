6741e720e02ea6da11cdd85a641bf16e3d230556
Tolotra <tolotor@yahoo.fr>
2022-07-12 08:28:49 +0300

    Comment bullet loaders component

---
+++ src/pages/PostPage/CommentBullet/CommentBulletLoaders.jsx
@@ -0,0 +1,7 @@
+import { BulletList } from "react-content-loader";
+
+const CommentBulletLoaders = () => {
+  return <BulletList style={{ maxWidth: "900px" }} />;
+};
+
+export default CommentBulletLoaders;
--- src/pages/PostPage/CommentBullet/index.jsx
+++ src/pages/PostPage/CommentBullet/index.jsx
@@ -1,3 +1,5 @@
 import CommentBullet from "./CommentBullet";
+import CommentBulletLoaders from "./CommentBulletLoaders";
 
 export default CommentBullet;
+export { CommentBulletLoaders };
