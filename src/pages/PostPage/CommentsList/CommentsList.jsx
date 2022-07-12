6755235f4acea569cb66cd49174c6b2bf5195806
Tolotra <tolotor@yahoo.fr>
2022-07-12 08:57:30 +0300

    Comments list component

---
+++ src/pages/PostPage/CommentsList/CommentsList.jsx
@@ -0,0 +1,41 @@
+import Spinner from "react-bootstrap/Spinner";
+
+// Components
+import CommentBullet, { CommentBulletLoaders } from "../CommentBullet";
+
+const CommentsList = ({ comments, page, isLoading, post, count, onCommentModified, onCommentDeleted }) => {
+  return isLoading && page === 1 ? (
+    <CommentBulletLoaders />
+  ) : (
+    <>
+      {count === 0 && (
+        <div className="empty-comments-placeholder d-flex flex-column align-items-center">
+          <p className="text-secondary text-center">Be the first to comment</p>
+        </div>
+      )}
+      {count > 0 && (
+        <>
+          <ul className="list-unstyled m-0 p-0">
+            {comments.map((comment) => (
+              <li key={`comment-${comment._id}`} className="mb-2">
+                <CommentBullet
+                  post={post}
+                  comment={comment}
+                  onCommentModified={onCommentModified}
+                  onCommentDeleted={onCommentDeleted}
+                />
+              </li>
+            ))}
+          </ul>
+          {isLoading && (
+            <div className="text-center">
+              <Spinner animation="border" variant="secondary" size="lg" />
+            </div>
+          )}
+        </>
+      )}
+    </>
+  );
+};
+
+export default CommentsList;
