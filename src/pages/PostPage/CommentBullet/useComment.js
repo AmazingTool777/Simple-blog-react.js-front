99e4257c87b4c2cfc11828705a5e5d4d3270b698
Tolotra <tolotor@yahoo.fr>
2022-07-17 10:44:31 +0300

    Comment modification submit handler

--- src/pages/PostPage/CommentBullet/useComment.js
+++ src/pages/PostPage/CommentBullet/useComment.js
@@ -1,5 +1,11 @@
 import { useState, useCallback } from "react";
 
+// API calls
+import { modifyPostComment } from "../../../apis/posts-api";
+
+// Other custom hooks
+import useCurrentUser from "../../../hooks/useCurrentUser";
+
 // Default callbacks arguments for the custom hook
 const defaultCallbacks = {
   onCommentModified: () => {},
@@ -9,16 +15,40 @@ const defaultCallbacks = {
 // Custom hook for the manipulation of individual comments
 export default function useComment(postId, comment, callbacks = defaultCallbacks) {
   const [content, setContent] = useState(comment.content);
-  const [isSubmitting] = useState(false);
+  const [isSubmitting, setSubmitting] = useState(false);
 
   const handleContentChange = useCallback((e) => setContent(e.target.value), []);
 
   const resetModifContent = useCallback(() => setContent(comment.content), [comment.content]);
+
+  const { currentUser } = useCurrentUser();
+
+  const { onCommentModified } = callbacks;
+
+  const handleCommentModifSubmit = useCallback(
+    async (e) => {
+      e.preventDefault();
+      const commentData = { content };
+      setSubmitting(true);
+      try {
+        const modifiedComment = await modifyPostComment(postId, comment._id, commentData);
+        // Setting the user field to the current user object
+        modifiedComment.user = currentUser;
+        setSubmitting(false);
+        onCommentModified(modifiedComment);
+      } catch (error) {
+        setSubmitting(false);
+        console.log(error);
+      }
+    },
+    [comment._id, content, onCommentModified, postId, currentUser]
+  );
 
   return {
     content,
     isSubmitting,
     handleContentChange,
     resetModifContent,
+    handleCommentModifSubmit,
   };
 }
